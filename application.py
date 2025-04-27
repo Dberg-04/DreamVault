476dbf7723855d81fada54a237076b06b36d55ca
Dberg-04 <dustyn.shelley@gmail.com>
2025-04-26 19:40:15 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,8 @@
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run()
 
